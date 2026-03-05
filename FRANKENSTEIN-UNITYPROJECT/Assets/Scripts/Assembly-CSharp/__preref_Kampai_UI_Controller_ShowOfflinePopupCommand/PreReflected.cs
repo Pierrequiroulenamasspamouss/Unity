bd405c081a90d546236c851324041be536a9348d
Pierrequiroulenamasspamouss <pierresluse@gmail.com>
2026-03-05 23:57:18 +0100

    fix ?

--- FRANKENSTEIN-UNITYPROJECT/Assets/Scripts/Assembly-CSharp/__preref_Kampai_UI_Controller_ShowOfflinePopupCommand/PreReflected.cs
+++ FRANKENSTEIN-UNITYPROJECT/Assets/Scripts/Assembly-CSharp/__preref_Kampai_UI_Controller_ShowOfflinePopupCommand/PreReflected.cs
@@ -8,11 +8,15 @@ namespace __preref_Kampai_UI_Controller_ShowOfflinePopupCommand
 			Constructor = (object[] p) => new global::Kampai.UI.Controller.ShowOfflinePopupCommand();
 			ConstructorParameters = global::System.Type.EmptyTypes;
 			PostConstructors = KampaiPreprocessedModule.EmptyPostConstructorsArray;
-			Setters = new global::System.Collections.Generic.KeyValuePair<global::System.Type, global::System.Action<object, object>>[3]
+			Setters = new global::System.Collections.Generic.KeyValuePair<global::System.Type, global::System.Action<object, object>>[4]
 			{
 				new global::System.Collections.Generic.KeyValuePair<global::System.Type, global::System.Action<object, object>>(typeof(bool), delegate(object target, object val)
 				{
 					((global::Kampai.UI.Controller.ShowOfflinePopupCommand)target).isShow = (bool)val;
+				}),
+				new global::System.Collections.Generic.KeyValuePair<global::System.Type, global::System.Action<object, object>>(typeof(global::Kampai.UI.View.SetupCanvasSignal), delegate(object target, object val)
+				{
+					((global::Kampai.UI.Controller.ShowOfflinePopupCommand)target).setupCanvasSignal = (global::Kampai.UI.View.SetupCanvasSignal)val;
 				}),
 				new global::System.Collections.Generic.KeyValuePair<global::System.Type, global::System.Action<object, object>>(typeof(global::strange.extensions.command.api.ICommandBinder), delegate(object target, object val)
 				{
@@ -23,7 +27,7 @@ namespace __preref_Kampai_UI_Controller_ShowOfflinePopupCommand
 					((global::Kampai.UI.Controller.ShowOfflinePopupCommand)target).injectionBinder = (global::strange.extensions.injector.api.IInjectionBinder)val;
 				})
 			};
-			SetterNames = new object[3];
+			SetterNames = new object[4];
 		}
 	}
 }
